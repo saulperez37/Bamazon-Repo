c2454d34dc9651491bdf29b8c7b0f3a9eef5be6e
saulperez37 <sperez_44@hotmail.com>
2019-11-03 23:58:18 -0700

    added cli table npm to display mysql table data. added functions for purchasing and updating item quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 let mysql = require("mysql");
+let Table = require('cli-table');
+let inquirer = require("inquirer");
 
-let connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -12,7 +14,74 @@ connection.connect(function (error) {
     if (error) {
         throw error;
     }
+
     console.log(`Successfully connected as id ${connection.threadId}`);
 
-    connection.end();
+    displayTable();
 });
+
+function displayTable() {
+    const sqlQuery = "SELECT * FROM products";
+
+    connection.query(sqlQuery, function (error, result) {
+        if (error) {
+            throw error;
+        }
+
+        let productTable = new Table({
+            head: ['Item ID', 'Product Name', 'Price'],
+            colWidths: [10, 30, 10]
+        });
+
+        for (let i = 0; i < result.length; i++) {
+            productTable.push([result[i].item_id, result[i].product_name, result[i].price]);
+        }
+
+        console.log(productTable.toString());
+        selectProduct();
+    });
+};
+
+function selectProduct() {
+    inquirer.prompt([
+        {
+            name: "item",
+            type: "input",
+            message: "Please enter the ID of the item you wish to purchase.",
+            filter: Number
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many of these items would you like to purchase.",
+            filter: Number
+        },
+    ])
+        .then(function (answers) {
+            let requestedItem = answers.item;
+            let requestedQuantity = answers.quantity
+            purchaseProduct(requestedItem, requestedQuantity);
+            // connection.end();
+        });
+};
+
+function purchaseProduct(item, qtyRequested) {
+    const sqlQuery = `SELECT * FROM products WHERE item_id = ${item}`;
+    connection.query(sqlQuery, function (error, results) {
+        if (error) {
+            throw error;
+        }
+        if (qtyRequested <= results[0].stock_quantity) {
+            let total = results[0].price * qtyRequested;
+            console.log(`This item is in stock!`);
+            console.log(`Your total cost ${qtyRequested} ${results[0].product_name} is ${total}.`);
+            console.log(`Thank You For Your Order!`);
+
+            connection.query(`UPDATE products SET stock_quantity = stock_quantity - ${qtyRequested} WHERE item_id = ${item}`);
+        } else {
+            console.log(`Sorry! We do not have enough ${results[0].product_name} to complete your order.`);
+        };
+        displayTable();
+    });
+};
+
